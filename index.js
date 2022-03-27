edea3c95ee43d568d430490f968deb39a2cb5b1e
pinky-banik <pinky.rani.banik@gmail.com>
2022-03-28 01:51:04 +0600

    env file added

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-const app = require('express')
-app 
+const express = require('express');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+const uri = "mongodb+srv://genius-car-mechanics:RF4oDpEv6gmGAoNp@cluster0.5f7tq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get ('/',(req,res)=>{
+    res.send(" Genius server is running");
+});
+
+
+app.listen(port,()=>{
+    console.log("Running on genius server on port", port);
+})
